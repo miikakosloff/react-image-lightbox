0d321fe8f991706b18cbe6e4b2b65ca73a0f10ae
Miika Kosloff <miika@folks.fi>
2020-03-06 10:59:50 +0200

    Sync rotation info

--- src/react-image-lightbox.js
+++ src/react-image-lightbox.js
@@ -27,6 +27,8 @@ import {
 } from './constant';
 import './style.css';
 
+//import RotateCW from './RotateCCW.svg'
+
 class ReactImageLightbox extends Component {
   static isTargetMatchImage(target) {
     return target && /ril-image-current/.test(target.className);
@@ -1688,7 +1690,19 @@ class ReactImageLightbox extends Component {
                         ? this.handleRotateCCWButtonClick
                         : undefined
                     }
-                  />
+                  >
+                    <svg
+                      viewBox="64 64 896 896"
+                      focusable="false"
+                      data-icon="redo"
+                      width="18px"
+                      height="18px"
+                      aria-hidden="true"
+                      style={{ fill: 'white', transform: 'rotate(90deg)' }}
+                    >
+                      <path d="M511.4 124C290.5 124.3 112 303 112 523.9c0 128 60.2 242 153.8 315.2l-37.5 48c-4.1 5.3-.3 13 6.3 12.9l167-.8c5.2 0 9-4.9 7.7-9.9L369.8 727a8 8 0 0 0-14.1-3L315 776.1c-10.2-8-20-16.7-29.3-26a318.64 318.64 0 0 1-68.6-101.7C200.4 609 192 567.1 192 523.9s8.4-85.1 25.1-124.5c16.1-38.1 39.2-72.3 68.6-101.7 29.4-29.4 63.6-52.5 101.7-68.6C426.9 212.4 468.8 204 512 204s85.1 8.4 124.5 25.1c38.1 16.1 72.3 39.2 101.7 68.6 29.4 29.4 52.5 63.6 68.6 101.7 16.7 39.4 25.1 81.3 25.1 124.5s-8.4 85.1-25.1 124.5a318.64 318.64 0 0 1-68.6 101.7c-7.5 7.5-15.3 14.5-23.4 21.2a7.93 7.93 0 0 0-1.2 11.1l39.4 50.5c2.8 3.5 7.9 4.1 11.4 1.3C854.5 760.8 912 649.1 912 523.9c0-221.1-179.4-400.2-400.6-399.9z"></path>
+                    </svg>
+                  </button>
                 </li>
               )}
               {enableRotation && (
@@ -1710,7 +1724,19 @@ class ReactImageLightbox extends Component {
                         ? this.handleRotateCWButtonClick
                         : undefined
                     }
-                  />
+                  >
+                    <svg
+                      viewBox="64 64 896 896"
+                      focusable="false"
+                      data-icon="redo"
+                      width="18px"
+                      height="18px"
+                      aria-hidden="true"
+                      style={{ fill: 'white', transform: 'rotate(-90deg)' }}
+                    >
+                      <path d="M758.2 839.1C851.8 765.9 912 651.9 912 523.9 912 303 733.5 124.3 512.6 124 291.4 123.7 112 302.8 112 523.9c0 125.2 57.5 236.9 147.6 310.2 3.5 2.8 8.6 2.2 11.4-1.3l39.4-50.5c2.7-3.4 2.1-8.3-1.2-11.1-8.1-6.6-15.9-13.7-23.4-21.2a318.64 318.64 0 0 1-68.6-101.7C200.4 609 192 567.1 192 523.9s8.4-85.1 25.1-124.5c16.1-38.1 39.2-72.3 68.6-101.7 29.4-29.4 63.6-52.5 101.7-68.6C426.9 212.4 468.8 204 512 204s85.1 8.4 124.5 25.1c38.1 16.1 72.3 39.2 101.7 68.6 29.4 29.4 52.5 63.6 68.6 101.7 16.7 39.4 25.1 81.3 25.1 124.5s-8.4 85.1-25.1 124.5a318.64 318.64 0 0 1-68.6 101.7c-9.3 9.3-19.1 18-29.3 26L668.2 724a8 8 0 0 0-14.1 3l-39.6 162.2c-1.2 5 2.6 9.9 7.7 9.9l167 .8c6.7 0 10.5-7.7 6.3-12.9l-37.3-47.9z"></path>
+                    </svg>
+                  </button>
                 </li>
               )}
 
@@ -1943,6 +1969,8 @@ ReactImageLightbox.propTypes = {
   // Aria-labels
   nextLabel: PropTypes.string,
   prevLabel: PropTypes.string,
+  rotateCWLabel: PropTypes.string,
+  rotateCCWLabel: PropTypes.string,
   zoomInLabel: PropTypes.string,
   zoomOutLabel: PropTypes.string,
   closeLabel: PropTypes.string,
@@ -1981,6 +2009,8 @@ ReactImageLightbox.defaultProps = {
   onRotateCCWRequest: () => {},
   onOpenImageRequest: () => {},
   prevLabel: 'Previous image',
+  rotateCWLabel: 'Rotate image clockwise',
+  rotateCCWLabel: 'Rotate image counterclockwise',
   prevSrc: null,
   prevRotation: 0,
   prevSrcThumbnail: null,
